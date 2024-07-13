7056efbabf208a8e80fb837053fbb3ae8c450f56
RitsuCloud <chonhinchou@gmail.com>
2024-07-13 10:02:09 -0700

    Initial Commit

---
+++ BowlingTracker/BowlingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingTrackerApp.swift
+//  BowlingTracker
+//
+//  Created by Chon Hin Chou on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BowlingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BowlingTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BowlingTracker
+//
+//  Created by Chon Hin Chou on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
