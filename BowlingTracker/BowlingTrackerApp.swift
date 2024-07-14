9bb8d5ec06ab59dee01d8c8299aa540cb16f54d8
RitsuCloud <chonhinchou@gmail.com>
2024-07-13 23:09:31 -0700

    newgame button added, no actual functionality yet.

--- BowlingTracker/BowlingTrackerApp.swift
+++ BowlingTracker/BowlingTrackerApp.swift
@@ -12,7 +12,6 @@ struct BowlingTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             SplashScreenView()
-            ContentView()
         }
     }
 }
--- BowlingTracker/ContentView.swift
+++ BowlingTracker/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+
         VStack {
-            Text("Testing!")
+            
+            Button {
+                newGameHandler();
+            } label: {
+//                Image(.newGame)
+//                    .resizable()
+//                    .frame(width: 120, height: 120, alignment: .bottom)
+                Text("New Game")
+                    .padding(20)
+                    .border(Color.purple)
+                    .position(x: 180, y: 700)
+            }
         }
         .padding()
+    }
+    
+    func newGameHandler() {
+        print("Just a placeholder")
     }
 }
 
 #Preview {
+    // We want the preview to be content view.
     ContentView()
-} 
+}
